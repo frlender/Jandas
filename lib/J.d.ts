ffe908bceaff0fc7ecd05877cf0ca87ee11b0c59
Qiaonan Duan <geonann@gmail.com>
2026-02-16 16:26:40 +0800

    1.5.0

--- lib/J.d.ts
+++ lib/J.d.ts
@@ -4,3 +4,4 @@ import DataFrame from './DataFrame';
 import { range } from './util';
 import { concat, from_raw, full } from './util2';
 export { DataFrame, Series, Index, range, concat, from_raw, full };
+export type { Obj, SeriesInitOptions, DataFrameInitOptions, DataFrameArrInitOptions, SortOptions, PushOptions, MergeOptions, SeriesRankOptions, DataFrameRankOptions, IndexRaw, SeriesRaw, DataFrameRaw, DropDuplicatesOptions, IndexType, QueryOptions, DiffOptions, RollingOptions, DataFrameRollingOptions } from './interfaces';
